49619dc46ad074315b0eddb9c7f80ba39e6c0f18
zm <gandaruvu118@gmail.com>
2021-02-09 19:01:14 +0800

    cros

--- desktop/get_clipboard_win.py
+++ desktop/get_clipboard_win.py
@@ -1,10 +1,16 @@
 from urllib import request, parse
 import os
-url = 'https://yourhost/get'
-params = {'secret': '63f369b84e1aec'}
+url = 'https://clipboard.ziiimo.cn:66/get'
+params = {'secret': '688cdcff00dd183'}
 
 data = bytes(parse.urlencode(params), encoding='utf8')
 response = request.urlopen(url, data=data)
 result = response.read().decode('utf-8')
-command = 'echo ' + result.strip() + '| clip'
+
+print(result)
+
+file_handle=open('tmp',mode='w')
+file_handle.write(result)
+file_handle.close()
+command = 'type tmp| clip'
 os.system(command)
--- desktop/send_clipboard.py
+++ desktop/send_clipboard.py
@@ -2,9 +2,9 @@ from tkinter import Tk
 from urllib import request, parse
 
 result = Tk().clipboard_get()
-url = 'https://yourhost/send'
+url = 'https://clipboard.ziiimo.cn:66/send'
 print(result)
-params = {'secret': '63f369b84e1aec', 'clipboard': result}
+params = {'secret': '688cdcff00dd183', 'clipboard': result}
 data = bytes(parse.urlencode(params), encoding='utf8')
 response = request.urlopen(url, data=data)
 result = response.read().decode('utf-8')
